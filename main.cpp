b82a0fc9c534652988144fa8f67f23fced7656ca
kabacsmate <kabacsmate@gmail.com>
2024-01-21 14:28:11 +0100

    Before first test saving
    Saved the project for copy to another folder in my computer.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,23 @@
 /* This is the main function of my Workflow chart creater application. */
 #include <stdio.h>
+//#include "todo_class.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // std::cout << "Itt meg vagyok" << std::endl;
+    // ToDo firstToDo("Elso tennivalo.", "Ezzel probald ki, hogy mukodik-e a konstruktor.", 420, 1);
+    // ToDo secondToDo;
+
+    // secondToDo.print();
+    // firstToDo.print();
+    // std::cout << "Am elek esku!" << std::endl;
+
+    int a = 2;
+    printf("sajt");
+    std::cout << "bármi szöveg" << std::endl;
+
+
+    return 0;
+}
--- todo_class.hpp
+++ todo_class.hpp
@@ -1,1 +1,45 @@
 /* This header files will contain my main class for the ToDo app */
+//#pragma once
+
+#include <iostream>
+#include <stdint.h>
+#include <string>
+
+
+class ToDo{
+    private:
+        static uint32_t idCounter;
+        uint32_t id;
+        std::string title;
+        std::string description;
+        uint32_t priority;
+        void* preconditions;
+        void* dependents;
+        time_t deadline;
+    public:
+        // Constructor
+        ToDo(std::string = nullptr, std::string = nullptr, uint32_t = 0, time_t = 0);
+
+        // Getter functions
+        uint32_t getID();
+        std::string getTitle();
+        std::string getDescription();
+        uint32_t getPriority();
+        void* getPreconditions();
+        void* getDependents();
+        time_t getDeadline();
+
+        // Setter functions
+        void setTitle(std::string);
+        void setDescription(std::string);
+        void setPriority(uint32_t);
+        void setPreconditions(void*);
+        void setDependents(void*);
+        void setDeadline(time_t);
+
+        // Other functions
+        void print(); // Print the whole Object
+
+};
+
+//uint32_t ToDo::idCounter = 0;
